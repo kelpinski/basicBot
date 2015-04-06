e243a2b2a013b70ef370df4524ce20402342dc20
Italo Soares <italocjs@live.com>
2015-04-05 22:57:13 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2229,7 +2229,7 @@
 
             killCommand: {
                 command: 'kill',
-                rank: 'bouncer',
+                rank: 'host',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
