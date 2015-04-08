e8623f48b1bf5b4fddcf1009c2b29fcb03eb2ada
Italo Soares <italocjs@live.com>
2015-04-08 09:26:47 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1,26 +1,3 @@
-Skip to content
- This repository
-Explore
-Gist
-Blog
-Help
-@italocjs italocjs
- 
- Unwatch 1
-  Star 0
-  Fork 275
-italocjs/basicBot
-forked from Yemasthui/basicBot
- tree: 6f5e2c3edb  basicBot/basicBot.js
-@italocjsitalocjs 2 days ago Update basicBot.js
-6 contributors @Yemasthui @Benzi @italocjs @ServePeak @WouterG @Hunchmun
-RawBlameHistory    3399 lines (3231 sloc)  166.385 kb
-/**
- *Copyright 2014 Yemasthui
- *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
- *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
- */
-
 
 (function () {
 
